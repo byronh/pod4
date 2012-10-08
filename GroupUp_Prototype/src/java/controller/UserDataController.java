98fd91318bc8eba0521fbe755920500e28464ea4
MD <MD@MD-PC>
2012-10-08 18:55:35 +0000

    Added mysql server based on demo, with basic user creation screen. Added a view that lists all users.

---
+++ GroupUp_Prototype/src/java/controller/UserDataController.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
+import model.UserData;
+
+/**
+ *
+ * @author MD
+ */
+@ManagedBean
+@SessionScoped
+public class UserDataController implements Serializable {
+
+    @PersistenceContext 
+    private EntityManager em;
+    
+    @Resource
+    private UserTransaction utx;
+    
+    private UserData userData = null;
+    
+    /**
+     * Creates a new instance of UserDataController
+     */
+    public UserDataController() {
+    }
+
+    public UserData getUserData() {
+        if( userData == null) {
+            userData = new UserData();
+        }
+        return userData;
+    }
+
+    public void setUserData(UserData userData) {
+        this.userData = userData;
+    }
+    
+    public String createUser() {
+        try {
+            utx.begin();
+            em.persist(this.userData);
+            utx.commit();
+            // return string of next webpage
+            return "userInformation";
+        } catch (Exception e) {        
+            // copy pasted this stuff, do sth about it later
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                                    "Error creating user!",
+                                                    "Unexpected error when creating your account.  Please contact the system Administrator");
+            Logger.getAnonymousLogger().log(Level.SEVERE,
+                                            "Unable to create new user",
+                                            e);
+            return null;
+        }
+    }
+
+    public void edit(UserData item) {
+        em.merge(item);
+    }
+
+    public void remove(UserData item) {
+        em.remove(em.merge(item));
+    }
+
+    public UserData find(Object id) {
+        return em.find(UserData.class, id);
+    }
+
+    public List<UserData> findAll() {
+        return em.createQuery("select object(o) from UserData as o").getResultList();
+    }
+
+    public List<UserData> findRange(int maxResults, int firstResult) {
+        Query q = em.createQuery("select object(o) from UserData as o");
+        q.setMaxResults(maxResults);
+        q.setFirstResult(firstResult);
+        return q.getResultList();
+    }
+
+    public int getUserDataCount() {
+        return ((Long) em.createQuery("select count(o) from UserData as o").getSingleResult()).intValue();
+    }
+}
---
+++ GroupUp_Prototype/src/java/model/UserData.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author MD
+ */
+@Entity
+public class UserData implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+    @ElementCollection
+    private List<String> friends = new ArrayList<String>();
+
+    public UserData() {
+    }
+    
+    public UserData(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UserData)) {
+            return false;
+        }
+        UserData other = (UserData) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.User[ id=" + id + " ]";
+    }
+    
+}
