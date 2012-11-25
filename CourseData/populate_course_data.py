c93979ecbe1b66f1cfde513d637fd38745887abf
byronh <byronh@gmail.com>
2012-11-25 02:10:32 -0800

    Fixed course data population script

--- CourseData/populate_course_data.py
+++ CourseData/populate_course_data.py
@@ -5,6 +5,7 @@
 # python populate_course_data.py
 
 import csv
+import datetime
 import MySQLdb
 
 # Connect to DB
@@ -22,7 +23,7 @@ try:
         if row[4] not in map(str, range(0,3)): # Not sure what to do with courses with term A,B,C,D. Ignore them?
             invalid_course_ids.append(row[0])
             continue
-        query = "INSERT INTO `groupup_course` (id, dept, coursenum, section, term) " + \
+        query = "INSERT INTO `groupup_course` (id, coursenum, dept, section, term) " + \
                 "VALUES (%s, %s, %s, %s, %s)"
         cursor.execute(query, tuple(row))
     db.commit()
@@ -32,10 +33,30 @@ try:
     i = 0
     for row in reader:
         i += 1
+
         if row[0] in invalid_course_ids:
             continue
+
+        try:
+            start_hour, start_minute = map(int, row[2].split(':'))
+        except ValueError:
+            start_minute = 0
+
+        start_offset = datetime.timedelta(hours=start_hour, minutes=start_minute)
+
+        try:
+            end_hour, end_minute = map(int, row[3].split(':'))
+        except ValueError:
+            end_minute = 0
+
+        end_offset = datetime.timedelta(hours=end_hour, minutes=end_minute)
+
+        row[2] = datetime.datetime.today() + start_offset
+        row[3] = datetime.datetime.today() + end_offset
+
         row.insert(0, i)
         row.append(1)
+        
         # Assume recurrence values: 0 = one-time, 1 = weekly, can add more later. All course timeslots are weekly.
         query = "INSERT INTO `groupup_timeslot` (id, course_id, day_of_week, start_time, end_time, reccurance) " + \
                 "VALUES (%s, %s, %s, %s, %s, %s)"
